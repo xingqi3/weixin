314d0922e19de698d6330a8087adf9dbb73d457d
星期三 <@DESKTOP-INUDOO8>
2019-07-03 21:18:14 +0800

    asd

--- start.sh
+++ start.sh
@@ -2,12 +2,15 @@
 
 mvn install
 
-cd ../cangk
+cd cangk
 mvn spring-boot:start
 
 cd ../subscribe
 mvn spring-boot:start
 
 cd ../unsubscribe
+mvn spring-boot:start
+
+cd ../self_menu
 mvn spring-boot:start
 
--- stop.sh
+++ stop.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 
-cd ../cangk
+cd cangk
 mvn spring-boot:stop
 
 cd ../subscribe
 mvn spring-boot:stop
 
 cd ../unsubscribe
+mvn spring-boot:stop
+
+cd ../self_menu
 mvn spring-boot:stop
 
